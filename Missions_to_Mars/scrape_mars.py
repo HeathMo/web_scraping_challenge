0dd902ebb81a843fdff5e003cdae08f0298e9ee8
HeathMo <heather.mott@yahoo.com>
2021-09-04 17:57:27 -0400

    added scrape mars py file

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,12 @@
+import pymongo
+import pandas as pd
+import requests
+import time
+import numpy as np
+import json
+from bs4 import BeautifulSoup
+from splinter import Browser
+from pprint import pprint
+from flask import Flask, render_template
+from selenium import webdriver
+
